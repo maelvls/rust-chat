a9d13dd4eab41e1374d94a6d9597a857b3cc428f
Maël Valais <mael.valais@gmail.com>
2018-03-04 16:23:52 +0100

    server can now talk

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![feature(iterator_try_fold)]
 #![recursion_limit = "1024"] // `error_chain!` can recurse deeply
 #[macro_use]
 extern crate clap;
@@ -18,7 +19,12 @@ use colored::*;
 // `error_chain!` creates.
 mod errors {
   // Create the Error, ErrorKind, ResultExt, and Result types
-  error_chain!{}
+  error_chain!{
+    // foreign_links {
+    //     Fmt(::std::fmt::Error);
+    //     Io(::std::io::Error) #[cfg(unix)];
+    // }
+  }
 }
 // This only gives access within this module. Make this `pub use errors::*;`
 // instead if the types must be accessible from other modules (e.g., within
@@ -51,32 +57,36 @@ fn main() {
     match args.subcommand() {
       ("server", Some(subarg)) => {
         let port = subarg.value_of("PORT").unwrap();
-        let listener = TcpListener::bind(format!("127.0.0.1:{}", port)).chain_err(|| {
-          format!(
-            "Port {} aleady used. Maybe server already running?",
-            port.yellow()
-          )
-        })?;
+        let listener = TcpListener::bind(format!("127.0.0.1:{}", port))
+          .chain_err(|| format!("port '{}' aleady used", port.yellow()))?;
         println!("{} listening started", "ready:".green().bold());
         for stream in listener.incoming() {
           println!("{} incoming connection", "note:".yellow().bold());
           let mut writer = stream.unwrap();
           let reader = writer.try_clone().unwrap();
-          // writer
-          thread::spawn(move || {
-            // 'move' because s_write will be mutated
+          // The writer.
+          // We use 'move' because 'writer' will be mutated. Each thread
+          // returns an Option<Error> so that we can know when they errored.
+          thread::spawn(move || -> Result<()> {
             writer
               .write(format!("{} ready!\r\n", "server:".purple()).as_bytes())
-              .unwrap();
-          });
-          // reader
-          thread::spawn(|| {
-            for b in reader.bytes() {
-              match b {
-                Err(e) => (println!("{} {:?}", "read error:".red().bold(), e)),
-                Ok(b) => print!("{}", b as char),
-              }
+              .chain_err(|| "could not write to client")?;
+            let stdin = std::io::stdin();
+            for b in stdin.bytes() {
+              let b = b.chain_err(|| "failed to read byte from stdin")?;
+              writer
+                .write(&[b])
+                .chain_err(|| format!("failed to write byte '{}'", b as char))?;
             }
+            Ok(())
+          });
+          // The reader.
+          thread::spawn(|| -> Result<()> {
+            for b in reader.bytes() {
+              let b = b.chain_err(|| "failed reading a byte")?;
+              print!("{}", b as char);
+            }
+            Ok(())
           });
         }
       }
@@ -92,45 +102,48 @@ fn main() {
           .chain_err(|| "impossibe to clone the TCP stream (i.e., the socket")?;
 
         println!("{} you can start typing", "ready:".green().bold());
-        // writer
-        let thd_read = thread::spawn(move || {
+        // The writer.
+        let thread_writer = thread::spawn(move || -> Result<()> {
           let stdin = std::io::stdin();
-          stdin.bytes().for_each(|b| {
-            let b = b.unwrap();
-            writer.write(&[b]).unwrap();
-          });
-        });
-        // reader
-        let thd_write = thread::spawn(|| {
-          for b in reader.bytes() {
-            match b.unwrap() as char {
-              b => print!("{}", b),
-            }
+          for b in stdin.bytes() {
+            let b = b.chain_err(|| "failed to read byte from stdin")?;
+            writer
+              .write(&[b])
+              .chain_err(|| format!("failed to write byte '{}'", b as char))?;
           }
+          Ok(())
         });
-        thd_write.join().unwrap();
-        thd_read.join().unwrap();
-        println!("{} both reader and writer started", "ready:".green().bold());
+        // The reader.
+        thread::spawn(|| -> Result<()> {
+          for b in reader.bytes() {
+            let b = b.chain_err(|| "failed to read a byte")?;
+            print!("{}", b as char)
+          }
+          Ok(())
+        });
+        // We must wait for the writing thread to terminate; otherwise,
+        // the program will quit immediately.
+        thread_writer
+          .join()
+          .unwrap()
+          .chain_err(|| "writing thread errored")?;
       }
       _ => panic!("tell the dev: 'clap' should have ensured a subcommand is given"),
     }
     Ok(())
   };
 
-  // run and handle errors
+  // Run and handle errors.
   if let Err(ref e) = run() {
     use std::io::Write;
     let stderr = &mut ::std::io::stderr();
     let errmsg = "Error writing to stderr";
-
     writeln!(stderr, "{} {}", "error:".red().bold(), e).expect(errmsg);
-
     for e in e.iter().skip(1) {
       writeln!(stderr, "{} {}", "caused by:".bright_black().bold(), e).expect(errmsg);
     }
+    // Use `RUST_BACKTRACE=1` to enable the backtraces.
     if let Some(backtrace) = e.backtrace() {
-      // The backtrace is not always generated. Try to run this example
-      // with `RUST_BACKTRACE=1`.
       writeln!(stderr, "{} {:?}", "backtrace:".blue().bold(), backtrace).expect(errmsg);
     }
     ::std::process::exit(1);
